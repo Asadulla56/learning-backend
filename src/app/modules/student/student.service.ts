2efa587143b2ea1705d9f84479f6be7fec4f1acb
Asadulla56 <mdasadullahoque294@gmail.com>
2025-11-02 12:58:11 +0600

    added students

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,31 +1,31 @@
 import { Student } from './student.model';
 import { IStudent } from './student.interface';
 
-// CREATE → নতুন student তৈরি
+// CREATE 
 const createStudentServices = async (payload: IStudent): Promise<IStudent> => {
   const student = await Student.create(payload);
   return student;
 };
 
-// READ → সব student data নেওয়া
+// READ 
 const getAllStudentsServices = async (): Promise<IStudent[]> => {
   const students = await Student.find();
   return students;
 };
 
-// READ (single) → একটি student খোঁজা by ID
+// READ (single)
 const getSingleStudentServices = async (id: string): Promise<IStudent | null> => {
   const student = await Student.findOne({ id });
   return student;
 };
 
-// UPDATE → student এর তথ্য আপডেট করা
+// UPDATE 
 const updateStudentServices = async (id: string, payload: Partial<IStudent>): Promise<IStudent | null> => {
   const updatedStudent = await Student.findOneAndUpdate({ id }, payload, { new: true });
   return updatedStudent;
 };
 
-// DELETE → একটি student ডিলিট করা
+// DELETE
 const deleteStudentServices = async (id: string): Promise<IStudent | null> => {
   const deletedStudent = await Student.findOneAndDelete({ id });
   return deletedStudent;
